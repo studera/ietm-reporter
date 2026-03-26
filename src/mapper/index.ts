a6e148b1569f7cf26a3096db840a351f2201f354
studera <robert_studera@chello.at>
2026-03-26 09:02:57 +0100

    test case mapping

---
+++ src/mapper/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Test Case Mapper Module
+ * Exports for mapping Playwright tests to IETM test cases
+ */
+
+export {
+  TestCaseMapper,
+  TestCaseMapping,
+  TestCaseMappingOptions,
+} from './TestCaseMapper';
+
+// Made with Bob
